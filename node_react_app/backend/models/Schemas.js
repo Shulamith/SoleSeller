35e1e9ff3797e382c3613841f732330543ece9e2
GuyotDavid <48332768+GuyotDavid@users.noreply.github.com>
2021-10-03 18:35:41 -0400

    Fixing some typos

--- node_react_app/backend/models/Schemas.js
+++ node_react_app/backend/models/Schemas.js
@@ -4,24 +4,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    // _id: ObjectId  (automaticlly created by mongoose everytime a doc is made)
-    username: {type:String, required:true},
-    fullname: {type: String, required:true},
-    entryDate: {type:Date, default:Date.now}
+    // _id: ObjectId  (automatically created by mongoose everytime a doc is made)
+    username: {type: String, required: true},
+    fullname: {type: String, required: true},
+    entryDate: {type: Date, default: Date.now}
 });
 
 const channelSchema = new Schema({
     // _id: ObjectId
-    channel: {type:String, required:true},
-    user: {type:Schema.Types.ObjectId, ref:'users'} // refrencing trough user id
+    channel: {type: String, required: true},
+    user: {type: Schema.Types.ObjectId, ref: 'users'} // referencing through user id
     // SELECT FROM a INNER JOIN b ON a.id = b.id
 });
 
 const itemSchema = new Schema ({
-    item: {type:String, reuired:true},
-    price: {type:String, reuired:true},
-    channel: {type:String, reuired:true},
-    user:{type:Schema.Types.ObjectId, ref:'users'} // refrences user table tru id
+    item: {type: String, required: true},
+    price: {type: String, required: true},
+    channel: {type: String, required: true},
+    user:{type: Schema.Types.ObjectId, ref: 'users'} // references user table tru id
 });
 
 
@@ -29,7 +29,7 @@ const Users = mongoose.model('users', userSchema, 'users');
 const Channels = mongoose.model('channels', channelSchema, 'channels')
 const Items = mongoose.model('items', itemSchema, 'items')
 
-const mySchemas = {'Users':Users, 'Channels':Channels, 'Items':Items};
+const mySchemas = {'Users': Users, 'Channels': Channels, 'Items': Items};
 
 // we can create multiple models of these schemas put it together as one and export it
 
