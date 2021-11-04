b26b28c6478a29259e7d3b43f2404482bbe8a3e2
GuyotDavid <48332768+GuyotDavid@users.noreply.github.com>
2021-11-04 13:50:22 -0400

    Add files via upload

---
+++ node_react_app/frontend/src/components/upload.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Link } from 'react-router-dom';
+import "./upload.css";
+
+class Upload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      itemName: '',
+      itemDescription: '',
+      EbayPrice: 0,
+      EtsyPrice: 0
+    };
+
+    this.onImageChange = this.onImageChange.bind(this);
+    this.onItemChange = this.onItemChange.bind(this);
+    this.onDescriptionChange = this.onDescriptionChange.bind(this);
+    this.onEbayChange = this.onEbayChange.bind(this);
+    this.onEtsyChange = this.onEtsyChange.bind(this);
+  }
+
+  onImageChange(e) {
+      this.setState({ image: e.target.value })
+  }
+
+  onItemChange(e) {
+    this.setState({ itemName: e.target.value })
+  } 
+
+  onDescriptionChange(e) {
+    this.setState({ itemDescription: e.target.value })
+  }
+
+  onEbayChange(e) {
+    this.setState({ EbayPrice: e.target.value })
+  }
+
+  onEtsyChange(e) {
+    this.setState({ EtsyPrice: e.target.value })
+  }
+
+  componentDidMount() {
+    this.userData = JSON.parse(localStorage.getItem('form'));
+
+    if (localStorage.getItem('form')) {
+        this.setState({
+            image: this.userData.image,
+            itemName: this.userData.itemName,
+            itemDescription: this.userData.itemDescription,
+            EbayPrice: this.userData.EbayPrice,
+            EtsyPrice: this.userData.EtsyPrice
+        })
+    } else {
+        this.setState({
+            image: null,
+            itemName: '',
+            itemDescription: '',
+            EbayPrice: 0,
+            EtsyPrice: 0
+        })
+    }
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('form', JSON.stringify(nextState));
+    }
+
+  render() {
+    const { image, itemName, itemDescription, EbayPrice, EtsyPrice } = this.state;
+    const isEnabled = itemName.length > 0 && itemDescription.length > 0 && EbayPrice > 0 && EtsyPrice > 0;
+    return (
+      <div className="Upload">
+        <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+                <label>Select Image</label>
+                <input type="file" accept=".jpg, .jpeg, .png" name="myImage" onChange={this.onImageChange} />
+            </div>
+            <div className="form-group">
+                <label>Item Name</label>
+                <input type="text" minLength="1" maxLength="50" className="form-control" value={this.state.itemName} onChange={this.onItemChange} />
+            </div>
+            <div class="form-group">
+                <label>Item Description</label>
+                <textarea class="form-control rounded-0" id="exampleFormControlTextarea1" rows="10" input type="text" minLength="1" maxLength="300" className="form-control" value={this.state.itemDescription} onChange={this.onDescriptionChange} />
+            </div>
+            <div className="form-group">
+                <label>Ebay Price</label>
+                <input type="number" min="0" step="any" className="form-control" value={this.state.EbayPrice} onChange={this.onEbayChange} />
+            </div>
+            <div className="form-group">
+                <label>Etsy Price</label>
+                <input type="number" min="0" step="any" className="form-control" value={this.state.EtsyPrice} onChange={this.onEtsyChange} />
+            </div>
+            <button id="sumbit" type="submit" className="btn btn-primary btn-block" disabled={!isEnabled}>
+                Submit
+            </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Upload;
