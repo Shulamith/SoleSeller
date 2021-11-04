25abad2de2687b636ed1e81453690bdfa4ba6e15
GuyotDavid <48332768+GuyotDavid@users.noreply.github.com>
2021-11-04 13:47:30 -0400

    Update Inventory.js

--- node_react_app/frontend/src/components/Inventory.js
+++ node_react_app/frontend/src/components/Inventory.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
-import "./Inventory.css";
+import './Inventory.css';
 
 function Inventory() {
 
@@ -17,27 +17,53 @@ function Inventory() {
         setItems(items);
     };
 
+    //some bootstrap to pretty things up
     return(
         <section>
+
+            <div className="container-fluid">
+                <h1 className="mt-5">Items</h1>
+                <form method="POST" action="/addItem">
+                    <div className="input-group justify-content-center">
+                            <div className="input-group-prepend">
+                                <input type="text" name="itemInput" className="form-control" placeholder="Item Name"/>
+                                <input type="text" name ="itemPriceInput" className="form-control" placeholder="Item Price"/>
+                                <input type="submit" value="Send" className="btn btn-primary mb-2"/>
+                            </div>
+                    </div>
+                </form>
+
+                {/* <form>
+                    <div className="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" placeholder="Enter email"/>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control"  placeholder="Password"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form> */}
+
             {
-                <div class="container">
-                    <div class="row row-cols-2">
-                        <div class="col">
+                <div className="container">
+                    <div className="row row-cols-2">
+                        <div className="col">
                             <p>Product 1</p>
                             <p>Price</p>
                             <p>Ebay</p>
                         </div>
-                        <div class="col">
+                        <div className="col">
                             Product 1
                             Price
                             Etsy
                         </div>
-                        <div class="col">
+                        <div className="col">
                             Product 2
                             Price
                             Ebay
                         </div>
-                        <div class="col">
+                        <div className="col">
                             Product 2
                             Price
                             Etsy
@@ -52,8 +78,9 @@ function Inventory() {
                     </div>
                 ))*/
             }
+            </div>
         </section>
-        
+
 
     );
 }
