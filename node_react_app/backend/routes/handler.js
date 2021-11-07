12c7d136b22f6486f67b21072f395fdcecb0e0ee
Ramon Torres <RTorres5318@gmail.com>
2021-11-07 12:40:36 -0500

    Merged all current frontend-related branches (inventory, inventory_update, frontend-branch). Added Etsy oauth functionality to backend.

--- node_react_app/backend/routes/handler.js
+++ node_react_app/backend/routes/handler.js
@@ -3,13 +3,81 @@ const router = express.Router();
 const EbayAuthToken = require('ebay-oauth-nodejs-client');
 const ebayAuthToken = new EbayAuthToken({
     filePath: './routes/ebay-config.json' // input file path.
-})
+});
+const fetch = require('cross-fetch');
 const bcrypt = require('bcrypt');
 const Schemas = require('../models/Schemas.js');
 
 router.use(express.urlencoded({ extended: false }));
 
 const users = []
+
+/* ------------------ BEGIN ETSY OAUTH ------------------ */
+
+// Send a JSON response to a default get request
+router.get('/ping', async (req, res) => {
+    const requestOptions = {
+        'method': 'GET',
+        'headers': {
+            'x-api-key': 'b397ddo9ov4lu91igrv1rjjc',
+        },
+    };
+
+    const response = await fetch(
+        'https://openapi.etsy.com/v3/application/openapi-ping',
+        requestOptions
+    );
+
+    if (response.ok) {
+        const data = await response.json();
+        res.send(data);
+    } else {
+        res.send("oops");
+    }
+});
+
+/**
+These variables contain our Etsy API Key, the state sent
+in the initial authorization request, and the client verifier compliment
+to the code_challenge sent with the initial authorization request
+*/
+const etsyClientID = 'b397ddo9ov4lu91igrv1rjjc';
+const etsyClientVerifier = 'dL5oT2IMlIV6zVXXRRaEk-OwbVGPKrlU0ids8Dg2ahk';
+const etsyRedirectUri = 'http://localhost:3000/oauth/redirect';
+
+router.get("/oauth/redirect", async (req, res) => {
+    // The req.query object has the query params that Etsy authentication sends
+    // to this route. The authorization code is in the `code` param
+    const authCode = req.query.code;
+    const tokenUrl = 'https://api.etsy.com/v3/public/oauth/token';
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify({
+            grant_type: 'authorization_code',
+            client_id: etsyClientID,
+            redirect_uri: etsyRedirectUri,
+            code: authCode,
+            code_verifier: etsyClientVerifier,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const response = await fetch(tokenUrl, requestOptions);
+
+    // Extract the access token from the response access_token data field
+    if (response.ok) {
+        const tokenData = await response.json();
+        res.redirect('/inventory');
+        //res.redirect(`/inventory?access_token=${tokenData.access_token}`);
+    } else {
+        res.send("oops");
+    }
+});
+
+/* ------------------ END ETSY OAUTH ------------------ */
+
 
 //GO http://localhost:4000/addUser TO ADD NEW USER WITH THIS CODE
 router.get('/addUser', async (req, res) => {
