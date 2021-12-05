d070eff5ea1012648d94058d06f60229489f3b0e
Shulamith <shulamit.dashevsky@gmail.com>
2021-12-04 19:40:51 -0500

    Removed unneccessary comments from index. js

--- node_react_app/backend/index.js
+++ node_react_app/backend/index.js
@@ -27,34 +27,6 @@ require('dotenv/config')
 
 
 
-// const clientScope = 'https://api.ebay.com/oauth/api_scope';
-// // // Client Crendential Auth Flow
-// ebayAuthToken.getApplicationToken('SANDBOX', clientScope).then((data) => {
-//     console.log(data);
-// }).catch((error) => {
-//     console.log(`Error to get Access token :${JSON.stringify(error)}`);
-// });
-
-// // Authorization Code Auth Flow
-// ebayAuthToken.generateUserAuthorizationUrl('SANDBOX', scopes); // get user consent url.
-
-// // // Exchange Code for Authorization token
-// ebayAuthToken.exchangeCodeForAccessToken('SANDBOX', code).then((data) => { // eslint-disable-line no-undef
-//     console.log(data);
-// }).catch((error) => {
-//     console.log(error);
-//     console.log(`Error to get Access token :${JSON.stringify(error)}`);
-// });
-
-// // // Getting access token from refresh token obtained from Authorization Code flow
-// const refreshToken = 'v^1.1#i^1#r^1#f^0#I^3#p^3#t^Ul4xMF8yOjNDMjU1MUI0OTJBMDg5NUZGMUY4RkEwNjk1MDRBQjQ2XzNfMSNFXjI2MA==';
-// ebayAuthToken.getAccessToken('PRODUCTION', refreshToken, scopes).then((data) => {
-//     console.log(data);
-// }).catch((error) => {
-//     console.log(`Error to get Access token from refresh token:${JSON.stringify(error)}`);
-// });
-
-
 // DB connection
 mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true})
 .then(() => {
@@ -63,20 +35,6 @@ mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:t
 .catch((err) => {
     console.log('err')
 })
-
-
-/*
-//WHAT IS THE CODE BELOW FOR????????
-// code for production
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'frontend/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'frontend/build', routesHandler));
-    });
-}
-*/
 
 
 //sslServer is necessary to use https instead of http
