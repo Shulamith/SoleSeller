473980cce4c8346578d058e1eacd1678374d67ad
Jason Cho <43969888+ja10109@users.noreply.github.com>
2021-12-12 20:29:41 -0500

    Update register.js

--- node_react_app/frontend/src/components/register.js
+++ node_react_app/frontend/src/components/register.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { Grid,TextField, Button, Typography,Link, InputAdornment } from '@material-ui/core'
+import test from './test.gif';
+import logo from './logo.png';
 import axios from 'axios';
+import { Person, AccountCircle, LockRounded } from "@material-ui/icons";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 export default class Register extends Component {
     userData;
@@ -66,16 +70,70 @@ render() {
     const avatarStyle={backgroundColor:'black'}
     const btnstyle={margin:'8px 0'}
     return(
-        <Grid>
-            <Paper elevation={5} style={paperStyle}>
-            <form onSubmit={this.onSubmit}>
-                <Grid align='center'>
-                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
-                    <h2>Register</h2>
-                </Grid>
-                <TextField label='Name' name='name' placeholder='Enter Name' value={this.state.name} onChange={this.onChangeName} fullWidth reuqired/>
-                <TextField label='Email' name='email' placeholder='Enter email' type='email' value={this.state.email} onChange={this.onChangeEmail} fullWidth required/>
-                <TextField label='Password' name='password' placeholder='Enter password' value={this.state.password} onChange={this.onChangePassword} type='password' fullWidth required/>
+        <Grid container style={{minHeight: '100vh'}}>
+            <Grid item xs={12} sm={6}>
+                <img src={test} 
+                style={{ width: '100%', height: '100%', objectFit: 'cover'}} 
+                alt="brand" />
+            </Grid>
+            <Grid container item xs={12} sm={6} alignItems="center" direction="column" justify="space-between" style={{padding:10}}>
+                <form onSubmit={this.onSubmit}>
+                <Card>
+                    <CardContent>
+                        <div style={{display:'flex', 
+                            flexDirection: "column", 
+                            maxWidth: 500, 
+                            minWidth: 300}}>
+                      <Grid container justify="center">
+                        <img src={logo}
+                        width={200}
+                        alt="logo"
+                        />
+                      </Grid>
+
+                      <h2>Welcome To Sole Seller</h2>
+                      <h2>Register as a New User</h2>
+                <TextField label='Name' 
+                            name='name' 
+                            placeholder='Enter Name' 
+                            value={this.state.name} 
+                            onChange={this.onChangeName} 
+                            InputProps={{
+                                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <Person />
+                                  </InputAdornment>
+                                ),
+                            }}                          
+                            fullWidth reuqired/>
+                <TextField label='Email' 
+                            name='email' 
+                            placeholder='Enter Email' 
+                            type='email' 
+                            value={this.state.email} 
+                            onChange={this.onChangeEmail} 
+                            InputProps={{
+                                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <AccountCircle />
+                                  </InputAdornment>
+                                ),
+                            }}
+                            fullWidth required/>
+                <TextField label='Password' 
+                            name='password' 
+                            placeholder='Enter Password' 
+                            value={this.state.password} 
+                            onChange={this.onChangePassword} 
+                            type='password' 
+                            InputProps={{
+                                startAdornment: (
+                                <InputAdornment position="start">
+                                  <LockRounded />
+                                </InputAdornment>
+                              ),
+                            }}
+                            fullWidth required/>
                 <Button type='submit' color='primary' variant="contained" style={btnstyle} disabled={!isEnabled} fullWidth>Register</Button>
                 <Typography > Already have an account?
                     <br></br>
@@ -83,9 +141,12 @@ render() {
                     Click Here To Login
                   </Link>
                 </Typography>
+                </div>
+                </CardContent>
+                </Card>
             </form>
-            </Paper>
         </Grid>
+    </Grid>
     )
 }
 }
